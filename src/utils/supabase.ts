7d3b43b973b586b6bda0684a6190fd6c93fb1958
Ogulcan <account.work@ogulcan.dev>
2024-06-15 15:56:43 +0300

    supabase package added

---
+++ src/utils/supabase.ts
@@ -0,0 +1,20 @@
+// React Native
+import "react-native-url-polyfill/auto";
+
+// AsyncStorage
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+// Supabase
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  process.env.EXPO_PUBLIC_SUPABASE_URL || "",
+  process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || "",
+  {
+    auth: {
+      storage: AsyncStorage,
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: false,
+    },
+});
